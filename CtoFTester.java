dc69573a090c699f41bf4f621f7f789664e20ff2
John Doe <rhossain30@stuy.edu>
2021-09-23 20:51:32 -0400

    added the c to f file

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,13 @@
-a
+public class CtoFTester {
+    public static double celsiusToFahrenheit(double c) {
+        return((c * (9/5)) + 32);
+    }
+    public static double fahrenheitToCelsius(double f) {
+        return((f-32)*5/9);
+    }
+    public static void main(String[] args) {
+        System.out.println(fahrenheitToCelsius(100));
+        System.out.println(celsiusToFahrenheit(100));
+    }
+
+}
